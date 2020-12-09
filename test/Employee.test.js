fc03855cf21403a3042d7c9f99310cbd9fc95127
Tskading <Tskading@gmail.com>
2020-12-08 20:03:23 -0800

    Passed employee tests.

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -6,43 +6,68 @@ test("Can instantiate Employee instance", () => {
 });
 
 test("Can set name via constructor arguments", () => {
+  // Arrange
   const name = "Alice";
+  // Act
   const e = new Employee(name);
+  // Assert
   expect(e.name).toBe(name);
 });
 
 test("Can set id via constructor argument", () => {
+  // Arrange
   const testValue = 100;
+  // Act
   const e = new Employee("Foo", testValue);
+  // Assert
   expect(e.id).toBe(testValue);
 });
 
 test("Can set email via constructor argument", () => {
+  // Arrange
   const testValue = "test@test.com";
+  // Act
   const e = new Employee("Foo", 1, testValue);
+  // Assert
   expect(e.email).toBe(testValue);
 });
 
 test("Can get name via getName()", () => {
+  // Arrange
   const testValue = "Alice";
   const e = new Employee(testValue);
-  expect(e.getName()).toBe(testValue);
+  // Act
+  const result = e.getName()
+  // Assert
+  expect(result).toBe(testValue);
 });
 
 test("Can get id via getId()", () => {
+  // Arrange
   const testValue = 100;
   const e = new Employee("Foo", testValue);
-  expect(e.getId()).toBe(testValue);
+  // Act
+  const result = e.getId()
+  // Assert
+  expect(result).toBe(testValue);
 });
 
 test("Can get email via getEmail()", () => {
+  // Arrange
   const testValue = "test@test.com";
   const e = new Employee("Foo", 1, testValue);
-  expect(e.getEmail()).toBe(testValue);
+  // Act
+  const result = e.getEmail()
+  // Assert
+  expect(result).toBe(testValue);
 });
 
 test("getRole() should return \"Employee\"", () => {
+  // Arrange
   const testValue = "Employee";
   const e = new Employee("Alice", 1, "test@test.com");
-  expect(e.getRole()).toBe(testValue);
+  // Act
+  const result = e.getRole()
+  // Assert
+  expect(result).toBe(testValue);
 });
--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,19 +1,30 @@
 const Engineer = require("../lib/Engineer");
 
 test("Can set GitHUb account via constructor", () => {
+  // Act
   const testValue = "GitHubUser";
   const e = new Engineer("Foo", 1, "test@test.com", testValue);
-  expect(e.github).toBe(testValue);
+  // Arrange
+  const result = e.github
+  // Assert
+  expect(result).toBe(testValue);
 });
 
 test("getRole() should return \"Engineer\"", () => {
+  // Act
   const testValue = "Engineer";
   const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
-  expect(e.getRole()).toBe(testValue);
+  // Arrange
+  const result = e.getRole()
+  // Assert
+  expect(result).toBe(testValue);
 });
 
 test("Can get GitHub username via getGithub()", () => {
+  // Act
   const testValue = "GitHubUser";
   const e = new Engineer("Foo", 1, "test@test.com", testValue);
-  expect(e.getGithub()).toBe(testValue);
+  // Arrange
+  const result = e.getGithub()
+  expect(result).toBe(testValue);
 });
